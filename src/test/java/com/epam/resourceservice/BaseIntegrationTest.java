05751f427eb04500caba0aa45ef3c52a1b14686d
Sherzod Omonov <sherzodbeek98@gmail.com>
2024-07-29 12:53:25 +0500

    test prop added

--- src/test/java/com/epam/resourceservice/BaseIntegrationTest.java
+++ src/test/java/com/epam/resourceservice/BaseIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.containers.localstack.LocalStackContainer;
@@ -16,6 +17,7 @@ import org.testcontainers.utility.DockerImageName;
 import java.time.Duration;
 
 @Testcontainers
+@TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class BaseIntegrationTest {
 
